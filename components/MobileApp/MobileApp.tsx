bde1fcf8a79e57b2b1fe207a5f00aeeccd3631e5
Darab <darabcode@gmail.com>
2022-11-07 18:09:33 +0100

    style: added befitting styles

--- components/MobileApp/MobileApp.tsx
+++ components/MobileApp/MobileApp.tsx
@@ -2,24 +2,67 @@ import { Box, Flex, Heading, Image, Stack, Text } from '@chakra-ui/react';
 
 const MobileApp = () => {
   return (
-    <Flex>
-      <Box backgroundColor="#f5faff">
-        <Image alt="mobile" src="/images/mobile.png" height={30} width={30} />
+    <Flex position="relative" marginTop="80px">
+      <Box
+        backgroundColor="#f5faff"
+        borderRadius="14px"
+        width="55%"
+        height="40rem"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        paddingLeft="110px"
+      >
+        <Image alt="mobile" src="/images/mobile.png" height={50} width={50} />
 
-        <Stack>
-          <Text color="#8a95ad">MOBILE APP</Text>
+        <Stack marginTop="20px">
+          <Text
+            color="#8a95ad"
+            fontWeight={500}
+            letterSpacing="5px"
+            lineHeight="20px"
+          >
+            MOBILE APP
+          </Text>
         </Stack>
-        <Heading>Do more on the go.</Heading>
-        <Stack>
-          <Text>Geegpay is coming soon to Google and Apple store!</Text>
+        <Heading
+          color="#152c5b"
+          fontSize="39px"
+          lineHeight="48px"
+          fontWeight={500}
+          marginTop="18px"
+        >
+          Do more on the go.
+        </Heading>
+        <Stack marginTop="20px">
+          <Text color="#8a95ad" fontSize="16px" lineHeight="24px">
+            Geegpay is coming soon to Google and Apple store!
+          </Text>
         </Stack>
-        <Box>
-          <Image src="/images/apple.png" alt="apple" />
-          <Image src="/images/playstore.png" alt="playstore" />
+        <Box display="flex" gap="20px" marginTop="25px">
+          <Image
+            src="/images/apple.png"
+            alt="apple"
+            height="41.7px"
+            width="140px"
+            cursor="pointer"
+          />
+          <Image
+            src="/images/playstore.png"
+            alt="playstore"
+            height="41.7px"
+            width="140px"
+            cursor="pointer"
+          />
         </Box>
       </Box>
-      <Box>
-        <Image src="/images/africa_lady.jpeg" alt="Africa Lady" />
+      <Box position="absolute" right="0" top="45px">
+        <Image
+          src="/images/africa_lady.jpeg"
+          alt="Africa Lady"
+          borderRadius="10px"
+          height="40rem"
+        />
       </Box>
     </Flex>
   );
