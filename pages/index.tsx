697151f052fa4ed8c1aba3d27e6834c69f586818
Darab <darabcode@gmail.com>
2022-11-25 07:35:44 +0100

    build: created faq section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Flex, Heading, Text } from '@chakra-ui/react';
 import DebitSection from '../components/debitSection/DebitSection';
+import FaqSections from '../components/faqSection/FaqSections';
 import DashboardHeader from '../components/layouts/dashboard/headers/DashboardHeader';
 import MainDashboard from '../components/layouts/dashboard/main/MainDashboard';
 import MobileApp from '../components/MobileApp/MobileApp';
@@ -35,6 +36,7 @@ const Home: NextPageWithLayout = () => {
       <VirtualAccount />
       <MobileApp />
       <DebitSection />
+      <FaqSections />
     </>
   );
 };
